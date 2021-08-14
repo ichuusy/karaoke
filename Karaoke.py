e766c057852d67b51e46a961c486238554c93b56
horisee <83366765+horisee@users.noreply.github.com>
2021-08-14 21:08:56 +0300

    Add files via upload

---
+++ Karaoke.py
@@ -0,0 +1,23 @@
+import os
+import speech_recognition as sr
+
+r = sr.Recognizer()
+clear = lambda: os.system('cls')
+lyrics,nowlyrics,worse = ["mousepad"],[],[]
+
+while len(lyrics) > 0:
+    with sr.Microphone() as source:
+        clear()
+        nowlyrics.append(lyrics[0])
+        print(f"Hatalı kelimeler : "+" ".join(worse)+"\n"+"Şarkı Sözü : "+" ".join(nowlyrics))
+        audio = r.listen(source)
+        try:
+            if r.recognize_google(audio) == lyrics[0]:
+                lyrics.pop(0)
+            else:
+                worse.append(lyrics[0])
+                lyrics.pop(0)
+        except:
+            print("HATA : Sesiniz algılanamadı.")
+            break
+print("Şarkıyı bitirdiniz. Level atlandı!")     
